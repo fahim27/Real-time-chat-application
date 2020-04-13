a08155061ec3e16e3c3d38dc8c642d50831f376e
fahim27 <fahimmbpi@gmail.com>
2020-04-14 02:18:54 +0600

    fahim

---
+++ resources/js/components/message.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+   				  <li class="list-group-item"><slot></slot></li>
+
+   </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
